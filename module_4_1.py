9e92fadc05e43282dfc4f4600163d89842795c1d
mik-305 <mik-305@bk.ru>
2024-08-21 09:31:41 +0300

    Add files via upload

--- module_4_1.py
+++ module_4_1.py
@@ -1,7 +1,12 @@
 from fake_math import divide as di_f
 from true_math import divide as di_t
 
-di_f(69,3)
-di_f(3,0)
-di_t(49,7)
-di_t(15,0)
+
+result1 = di_f(69, 3)
+result2 = di_f(3, 0)
+result3 = di_t(49, 7)
+result4 = di_t(15, 0)
+print('result1 = ',result1)
+print('result2 = ',result2)
+print('result3 = ',result3)
+print('result4 = ',result4)
